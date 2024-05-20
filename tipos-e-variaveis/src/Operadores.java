11a5c17fc411ae2a9c94c1493c12e21fb0457eea
Ednario Andrade <contatoednario@gmail.com>
2024-05-20 14:46:19 -0300

    feat: Operadores

---
+++ tipos-e-variaveis/src/Operadores.java
@@ -0,0 +1,13 @@
+public class Operadores {
+  public static void main(String[] args) {
+    int a, b;
+
+    a = 6;
+    b = 6;
+
+    // expressao de if e else abreviado
+    String resultado = a == b ? "Verdadeiro" : "Falso";
+    
+    System.err.println(resultado);
+  }
+}
