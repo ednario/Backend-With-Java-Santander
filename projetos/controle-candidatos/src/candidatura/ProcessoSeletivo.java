c63f6f533750b588a7b7d1572da1442acb908dd8
Ednario Andrade <contatoednario@gmail.com>
2024-06-18 10:14:13 -0300

    feat: Add contact attempt logic for candidates

--- projetos/controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ projetos/controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,9 +1,39 @@
 package candidatura;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;;
 
 public class ProcessoSeletivo {
   public static void main(String[] args) {
-    imprimirSelecionados();
+    String [] candidatos = {"João", "Maria", "Pedro", "Augusto", "Antonio"};
+    for (String candidato: candidatos) {
+      entrandoEmContato(candidato);
+    }
+  }
+
+  static void entrandoEmContato (String candidato) {
+    int tentativasRealizadas = 1;
+    boolean continuarTentando = true;
+    boolean atendeu = false;
+
+    do {
+      atendeu = atender();
+      continuarTentando = !atendeu;
+      if(continuarTentando) {
+        tentativasRealizadas++;
+      } else {
+        System.out.println("Contato realizado com sucesso");
+      }
+    } while (continuarTentando && tentativasRealizadas < 3);
+
+    if (atendeu) {
+      System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + "° TENTATIVA");
+    } else {
+      System.out.println("NAO CONSEGUIMOS CONTATO COM " + candidato);
+    }
+  }
+
+  static boolean atender() {
+    return new Random().nextInt(3)==1;
   }
 
   static void imprimirSelecionados () {
