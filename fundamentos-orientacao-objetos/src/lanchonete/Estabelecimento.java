924a18a8307d61e4e9e5c7befc4781dd9f9be97a
Ednario Andrade <contatoednario@gmail.com>
2024-06-25 18:51:35 -0300

    feat: Refactor Estabelecimento class and remove unnecessary code

--- fundamentos-orientacao-objetos/src/lanchonete/Estabelecimento.java
+++ fundamentos-orientacao-objetos/src/lanchonete/Estabelecimento.java
@@ -3,7 +3,6 @@ package lanchonete;
 public class Estabelecimento {
 	public static void main(String[] args) {
 		Cozinheiro cozinheiro = new Cozinheiro();
-		//ações que não precisam estarem disponíveis para toda a aplicação
 		cozinheiro.lavarIngredientes();
 		cozinheiro.baterVitaminaLiquidificador();
 		cozinheiro.selecionarIngredientesVitamina();
@@ -11,16 +10,13 @@ public class Estabelecimento {
 		cozinheiro.prepararVitamina();
 		cozinheiro.prepararVitamina();
 		
-		//ações que estabelecimento precisa ter ciência
 		cozinheiro.adicionarSucoNoBalcao();;
 		cozinheiro.adicionarLancheNoBalcao();
 		cozinheiro.adicionarComboNoBalcao();
 		
 		Almoxarife almoxarife = new Almoxarife();
-		//ações que não precisam estarem disponíveis para toda a aplicação
 		almoxarife.controlarEntrada();
 		almoxarife.controlarSaida();
-		//ação que somente o seu pacote cozinha precisa conhecer (default)
 		almoxarife.entregarIngredientes();
 		almoxarife.trocarGas();
 		
@@ -28,7 +24,6 @@ public class Estabelecimento {
 		atendente.pegarLancheCozinha();
 		atendente.receberPagamento();
 		atendente.servindoMesa();
-		//ação que somente o seu pacote cozinha precisa conhecer (default)
 		atendente.trocarGas();
 		
 		Cliente cliente = new Cliente();
@@ -36,14 +31,10 @@ public class Estabelecimento {
 		cliente.fazerPedido();
 		cliente.pagarConta();
 		
-		//não deveria, mas o estabelecimento
-		//ainda não definiu normas de atendimento
 		cliente.pegarPedidoBalcao();
 		
-		//esta ação é muito sigilosa, qua tal ser privada ?
 		cliente.consultarSaldoAplicativo();
 		
-		//já pensou os clientes ouvindo que o gás acabou ?
 		cozinheiro.pedirParaTrocarGas(atendente);
 		cozinheiro.pedirParaTrocarGas(almoxarife);
 		
