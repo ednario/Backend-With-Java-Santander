2975f60752020ad8de2a4424df1b81785af83609
Ednario Andrade <contatoednario@gmail.com>
2024-06-26 20:51:22 -0300

    feat: Refactor Estabelecimento class and remove unnecessary code

--- fundamentos-orientacao-objetos/src/lanchonete/Estabelecimento.java
+++ fundamentos-orientacao-objetos/src/lanchonete/Estabelecimento.java
@@ -1,42 +1,26 @@
 package lanchonete;
 
+import lanchonete.area_cliente.Cliente;
+import lanchonete.atendimento.Atendente;
+import lanchonete.atendimento.cozinha.Cozinheiro;
+
 public class Estabelecimento {
 	public static void main(String[] args) {
 		Cozinheiro cozinheiro = new Cozinheiro();
-		cozinheiro.lavarIngredientes();
-		cozinheiro.baterVitaminaLiquidificador();
-		cozinheiro.selecionarIngredientesVitamina();
-		cozinheiro.prepararLanche();
-		cozinheiro.prepararVitamina();
-		cozinheiro.prepararVitamina();
 		
 		cozinheiro.adicionarSucoNoBalcao();;
 		cozinheiro.adicionarLancheNoBalcao();
 		cozinheiro.adicionarComboNoBalcao();
 		
-		Almoxarife almoxarife = new Almoxarife();
-		almoxarife.controlarEntrada();
-		almoxarife.controlarSaida();
-		almoxarife.entregarIngredientes();
-		almoxarife.trocarGas();
-		
 		Atendente atendente = new Atendente();
-		atendente.pegarLancheCozinha();
 		atendente.receberPagamento();
 		atendente.servindoMesa();
-		atendente.trocarGas();
-		
+
 		Cliente cliente = new Cliente();
 		cliente.escolherLanche();
 		cliente.fazerPedido();
 		cliente.pagarConta();
 		
-		cliente.pegarPedidoBalcao();
-		
 		cliente.consultarSaldoAplicativo();
-		
-		cozinheiro.pedirParaTrocarGas(atendente);
-		cozinheiro.pedirParaTrocarGas(almoxarife);
-		
 	}
 }
