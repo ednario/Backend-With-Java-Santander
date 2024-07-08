dc8f55bc826eac271330ca8b9546b161647ae6db
Ednario Andrade <contatoednario@gmail.com>
2024-07-07 21:09:34 -0300

    feat: Add ListaTarefa class to the OperacoesBasicas package

---
+++ collections/list-interface/src/OperacoesBasicas/ListaTarefa.java
@@ -0,0 +1,50 @@
+package OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+  // atributos
+  private List<Tarefa> tarefaList;
+
+  public ListaTarefa() {
+    this.tarefaList = new ArrayList<>();
+  }
+
+  public void adicionarTarefa(String descricao) {
+    tarefaList.add(new Tarefa(descricao));
+  }
+
+  public void removerTarefa(String descricao) {
+    List<Tarefa> tarefasParaRemover = new ArrayList<>();
+
+    for (Tarefa t : tarefaList) {
+      if(t.getDescricao().equalsIgnoreCase(descricao)) {
+        tarefasParaRemover.add(t);
+      }
+    }
+    tarefaList.removeAll(tarefasParaRemover);
+  } 
+
+  public int obterNumeroTotalTarefas () {
+    return tarefaList.size();
+  }
+
+  public void obterDescricoesTarefas() {
+    System.out.println(tarefaList);
+  }
+
+  public static void main(String[] args) {
+    ListaTarefa listaTarefa = new ListaTarefa();
+
+    listaTarefa.adicionarTarefa("Tarefa 01");
+    listaTarefa.adicionarTarefa("Tarefa 02");
+    listaTarefa.adicionarTarefa("Tarefa 02");
+
+    listaTarefa.removerTarefa("Tarefa 02");
+
+    System.out.println("O numero total de tarefas e: " + listaTarefa.obterNumeroTotalTarefas());
+    
+    listaTarefa.obterDescricoesTarefas();
+  }
+}
