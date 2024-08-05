5710525f4281d5416ecf8ca1eb22801a191f52be
Ednario Andrade <contatoednario@gmail.com>
2024-08-04 21:06:25 -0300

    feat: Add spring application configuration files and classes for managing bank accounts

---
+++ spring/aprendendo/src/main/java/com/ednario/value/AprendendoApplication.java
@@ -0,0 +1,14 @@
+package com.ednario.value;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendendoApplication.class, args);
+
+		System.out.println("Hello World!");
+	}
+}
---
+++ spring/aprendendo/src/test/java/com/ednario/aprendendo/AprendendoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ednario.aprendendo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprendendoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
