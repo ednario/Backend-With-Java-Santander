2b2e487b17d842fc5ccd49782d5c1dec6177d5b5
Ednario Andrade <contatoednario@gmail.com>
2024-08-04 21:44:31 -0300

    feat: @Value

--- spring/aprendendo/src/main/java/com/ednario/value/PrincipalApplication.java
+++ spring/aprendendo/src/main/java/com/ednario/value/PrincipalApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AprendendoApplication {
+public class PrincipalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AprendendoApplication.class, args);
-
-		System.out.println("Hello World!");
+		SpringApplication.run(PrincipalApplication.class, args);
 	}
 }
---
+++ spring/aprendendo/src/main/java/com/ednario/value/app/SistemaMensagem.java
@@ -0,0 +1,27 @@
+package com.ednario.value.app;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SistemaMensagem implements CommandLineRunner {
+  
+  @Value("${nome}")
+  private String nome;
+  @Value("${email}")
+  private String email;
+  @Value("${telefones}")
+  private List<Long> telefones;
+
+  @Override
+  public void run(String... args) throws Exception {
+    System.out.println(
+      "Mensagem enviada por: " + nome 
+       + "\nEmail: " + email 
+       + "\nTelefones: " + telefones);
+    System.out.println("Seu cadastro foi realizado com sucesso!");
+  }
+}
